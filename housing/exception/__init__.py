57124f542cafec723ab4f9a6909fa0e10e20053c
DeeptiMish <dem122333@gmail.com>
2022-07-18 23:34:05 +0530

    final update

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -19,7 +19,8 @@ class HousingException(Exception):
         error_detail: object of sys module
         """
         _,_ ,exec_tb = error_detail.exc_info()
-        line_number = exec_tb.tb_frame.f_lineno
+        exception_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number=exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
         error_message = f"Error occured in scrip: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
         return error_message
